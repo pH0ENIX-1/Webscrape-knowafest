66b64dbf82df0e5617f510b589f5a010908b6dc1
Sanjayan <63048718+pH0ENIX-1@users.noreply.github.com>
2023-03-12 13:19:14 +0530

    refactor code - 2

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -1,42 +1,51 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import json
 
 class Scraper:
     
-    def __init__(self, text_filename :str = "hackathons.txt", xlsx_filename :str = "hackathon_data.txt"):
-        self.file_name = text_filename
-        self.xlsx_name = xlsx_filename
-
-    def request_site(self, url = "https://www.knowafest.com/explore/category/Hackathon"):
+    def __init__(self, text_filename :str = "hackathons.txt", xlsx_filename :str = "hackathon_data.xlsx"):
         
-        html_file = requests.get(url).text
-        with open('site.html','w') as website:
+        setting_handler = open("settings.json")
+        settings = json.load(setting_handler)
+        
+        self.file_name = settings["text"]
+        self.xlsx_name = settings["xlsx"]
+        self.html_name = settings["html"]
+        self.url = settings["url"]
+
+    def request_site(self):
+        
+        html_file = requests.get(self.url).text
+        with open(self.html_name,'w') as website:
             website.write(html_file)
 
     def preprocess_data(self, row_data:str):
         
         row_data = row_data.split('\n')
         row_data.pop(4)
+        
         for element in row_data:
             if element == '':
                 row_data.remove(element)
+        
         return row_data
 
     def write_data(self):   
         
-        with open('site.html','r') as website:
+        with open(self.html_name,'r') as website:
             
             soup = BeautifulSoup(website,'lxml')
             hackathons = soup.find_all('tr')
             locations = soup.find_all("span", itemprop = "name")
             register_links = soup.find_all("tr", itemtype="http://schema.org/Event")
             print(register_links[0].get("onClick"))
-            with open('hackathons.txt','w') as outfile:
+            
+            with open(self.file_name,'w') as outfile:
                 
                 dataframe_columns = hackathons.pop(0).text.split("\n")
                 dataframe_columns.pop(4)
-                # print(dataframe_columns)
                 self.hack_data = pd.DataFrame(columns = dataframe_columns)
                 
                 for index in range(len(hackathons)):
@@ -48,12 +57,11 @@ class Scraper:
 
                     row_data = self.preprocess_data(hackathon)
                     row_data.append(location)
-                    # print(row_data)
 
                     self.hack_data.loc[len(self.hack_data.index)] = row_data
                 
                 print(self.hack_data)
-                self.hack_data.to_excel("hackathon_data.xlsx")
+                self.hack_data.to_excel(self.xlsx_name)
 
     def scrape_data(self):
         
